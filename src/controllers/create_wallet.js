7243bf52e6787d62770959223729327a8039a19e
nol4lej <nome.ahumada@outlook.com>
2023-06-06 03:49:28 -0400

    Design + UX

--- src/controllers/create_wallet.js
+++ src/controllers/create_wallet.js
@@ -1,14 +1,41 @@
 import { web3 } from "./1connections.js";
 import { handleChainChanged } from "./5chains.js";
 
-const create_wallet = document.getElementById("create_wallet")
+const create_wallet = document.getElementById("create_wallet");
+const public_key = document.getElementById("public_key");
+const private_key = document.getElementById("private_key");
+const wallets_created = document.getElementById("wallets_created");
+const copy_public_key = document.getElementById("copy_public_key");
+const copy_private_key = document.getElementById("copy_private_key");
+
+let new_wallet;
+
 async function createWallet(){
     await handleChainChanged();
-    const new_wallet = web3.eth.accounts.create();
-    console.log("Address:",new_wallet.address)
-    console.log("Private key:",new_wallet.privateKey)
+    new_wallet = web3.eth.accounts.create();
+    wallets_created.style.display = "flex";
+    public_key.textContent = new_wallet.address.slice(0,6)+"..."+new_wallet.address.slice(-4);
+    private_key.textContent = new_wallet.privateKey.slice(0,6)+"..."+new_wallet.privateKey.slice(-4);
 }
 
 create_wallet.onclick = createWallet;
+
+copy_public_key.addEventListener("click", () => {
+    navigator.clipboard.writeText(new_wallet.address)
+    // const copied_hash = document.getElementById("copied_hash");
+    // copied_hash.classList.add("active")
+    // setTimeout(function() {
+    //     copied_hash.classList.remove('active');
+    //   }, 1000);
+})
+
+copy_private_key.addEventListener("click", () => {
+    navigator.clipboard.writeText(new_wallet.privateKey)
+    // const copied_hash = document.getElementById("copied_hash");
+    // copied_hash.classList.add("active")
+    // setTimeout(function() {
+    //     copied_hash.classList.remove('active');
+    //   }, 1000);
+})
 
 export {createWallet}
